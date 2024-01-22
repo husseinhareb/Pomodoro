905ed64be32234a073fe7f211805ba6b40e0c152
husseinhareb <shtam.hareb12@gmail.com>
2024-01-22 11:32:44 +0200

    first commit

--- testing.js
+++ testing.js
@@ -8,9 +8,15 @@ console.log(hello('hussein','hareb'));
 
 const arrayidk = [1,2,3,4,5,6];
 
-premier = (number) => {
+evenOrOdd = (number) => {
     if(number%2)
     {
-        return 'Od'
+        return 'Odd'
     }
+    return 'Even';
 }
+
+const newList = arrayidk.map(item => evenOrOdd(item));
+
+console.log(newList);
+
