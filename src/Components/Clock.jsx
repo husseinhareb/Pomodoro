b8383296f336ff47eaf835a80120084e3d45e2bb
husseinhareb <shtam.hareb12@gmail.com>
2024-01-22 12:32:44 +0200

    first commit

---
+++ src/Components/Clock.jsx
@@ -0,0 +1,10 @@
+import React,{useState,useEffect,useRef} from "react";
+
+function Clock(){
+    const [time,setTime] = useState(new Date());
+    const update = useRef;
+
+    useEffect = () =>{
+        
+    }
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+function Navbar()
+{
+
+    return (
+        <nav className="">
+            <ul>
+                <li>Pomodoro</li>
+                <li></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
