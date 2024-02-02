707e215ff1901ed217033c515a5eb8dd5034c89f
husseinhareb <shtam.hareb12@gmail.com>
2024-02-02 17:06:35 +0200

    Initialize project using Create React App

---
+++ testing.js
@@ -0,0 +1,16 @@
+hello = (first,last) =>{
+    return "Hello " + first + " " + last;
+}
+
+
+console.log(hello('hussein','hareb'));
+
+
+const arrayidk = [1,2,3,4,5,6];
+
+premier = (number) => {
+    if(number%2)
+    {
+        return 'Od'
+    }
+}
