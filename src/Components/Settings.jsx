02daf0d465c306aa9877dc62de8b529bdd04a48c
husseinhareb <shtam.hareb12@gmail.com>
2024-02-28 13:23:49 +0200

    settingsWindow

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -3,26 +3,58 @@ import Cookies from "js-cookie";
 
 function Settings({ onClose }) {
   const [pomodoroTime, setPomodoroTime] = useState({ minutes: 25, seconds: 0 });
+  const [shortBreakTime, setShortBreakTime] = useState({ minutes: 5, seconds: 0 });
+  const [longBreakTime, setLongBreakTime] = useState({ minutes: 15, seconds: 0 });
 
   const handleClose = () => {
-    onClose(pomodoroTime);
+    onClose({ pomodoroTime, shortBreakTime, longBreakTime });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     handleClose();
     Cookies.set("pomodoroTime", JSON.stringify(pomodoroTime), { sameSite: "None", secure: true });
+    Cookies.set("shortBreakTime", JSON.stringify(shortBreakTime), { sameSite: "None", secure: true });
+    Cookies.set("longBreakTime", JSON.stringify(longBreakTime), { sameSite: "None", secure: true });
   };
 
-  const handleMinutesChange = (e) => {
+  const handlePomodoroMinutesChange = (e) => {
     setPomodoroTime((prevTime) => ({
       ...prevTime,
       minutes: parseInt(e.target.value) || 0
     }));
   };
 
-  const handleSecondsChange = (e) => {
+  const handlePomodoroSecondsChange = (e) => {
     setPomodoroTime((prevTime) => ({
+      ...prevTime,
+      seconds: parseInt(e.target.value) || 0
+    }));
+  };
+
+  const handleShortBreakMinutesChange = (e) => {
+    setShortBreakTime((prevTime) => ({
+      ...prevTime,
+      minutes: parseInt(e.target.value) || 0
+    }));
+  };
+
+  const handleShortBreakSecondsChange = (e) => {
+    setShortBreakTime((prevTime) => ({
+      ...prevTime,
+      seconds: parseInt(e.target.value) || 0
+    }));
+  };
+
+  const handleLongBreakMinutesChange = (e) => {
+    setLongBreakTime((prevTime) => ({
+      ...prevTime,
+      minutes: parseInt(e.target.value) || 0
+    }));
+  };
+
+  const handleLongBreakSecondsChange = (e) => {
+    setLongBreakTime((prevTime) => ({
       ...prevTime,
       seconds: parseInt(e.target.value) || 0
     }));
@@ -38,23 +70,65 @@ function Settings({ onClose }) {
       <form onSubmit={handleSubmit}>
         <div>
           <h3>Pomodoro</h3>
-          <label htmlFor="minutes">Minutes:</label>
+          <label htmlFor="pomodoro-minutes">Minutes:</label>
           <input
             type="number"
-            id="minutes"
+            id="pomodoro-minutes"
             placeholder="25"
             className="minutes"
             value={pomodoroTime.minutes}
-            onChange={handleMinutesChange}
+            onChange={handlePomodoroMinutesChange}
           />
-          <label htmlFor="seconds">Seconds:</label>
+          <label htmlFor="pomodoro-seconds">Seconds:</label>
           <input
             type="number"
-            id="seconds"
+            id="pomodoro-seconds"
             placeholder="00"
             className="seconds"
             value={pomodoroTime.seconds}
-            onChange={handleSecondsChange}
+            onChange={handlePomodoroSecondsChange}
+          />
+        </div>
+        <div>
+          <h3>Short Break</h3>
+          <label htmlFor="shortbreak-minutes">Minutes:</label>
+          <input
+            type="number"
+            id="shortbreak-minutes"
+            placeholder="5"
+            className="minutes"
+            value={shortBreakTime.minutes}
+            onChange={handleShortBreakMinutesChange}
+          />
+          <label htmlFor="shortbreak-seconds">Seconds:</label>
+          <input
+            type="number"
+            id="shortbreak-seconds"
+            placeholder="00"
+            className="seconds"
+            value={shortBreakTime.seconds}
+            onChange={handleShortBreakSecondsChange}
+          />
+        </div>
+        <div>
+          <h3>Long Break</h3>
+          <label htmlFor="longbreak-minutes">Minutes:</label>
+          <input
+            type="number"
+            id="longbreak-minutes"
+            placeholder="15"
+            className="minutes"
+            value={longBreakTime.minutes}
+            onChange={handleLongBreakMinutesChange}
+          />
+          <label htmlFor="longbreak-seconds">Seconds:</label>
+          <input
+            type="number"
+            id="longbreak-seconds"
+            placeholder="00"
+            className="seconds"
+            value={longBreakTime.seconds}
+            onChange={handleLongBreakSecondsChange}
           />
         </div>
         <button type="submit">Ok</button>
